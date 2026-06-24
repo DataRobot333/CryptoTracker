7a99eb77e3ce7539acafca74b565adad27232f2b
datarobot333 <ds.ar2049@gmail.com>
2026-06-24 17:31:48 +0330

    add sql files

---
+++ sql/008_OHLC_each_coin.sql
@@ -0,0 +1,31 @@
+with daily_stat as (
+SELECT
+    coin_id,
+    DATE(collection_time) as trade_day,
+    MIN(collection_time) as first_time,
+    MAX(collection_time) as last_time,
+    max(price_usd) as high_price,
+    MIN(price_usd) as low_price
+from market_snapshots
+GROUP BY coin_id, DATE(collection_time)
+)
+
+SELECT
+    ds.coin_id,
+    ds.trade_day,
+    open_ms.price_usd as open_price,
+    ds.high_price,
+    ds.low_price,
+    close_ms.price_usd as close_price
+
+from daily_stat as ds
+
+JOIN market_snapshots open_ms
+    on open_ms.coin_id = ds.coin_id
+    and open_ms.collection_time = ds.first_time
+
+JOIN market_snapshots close_ms
+    on close_ms.coin_id = ds.coin_id
+    and close_ms.collection_time = ds.last_time
+
+ORDER BY ds.trade_day DESC, ds.coin_id;
